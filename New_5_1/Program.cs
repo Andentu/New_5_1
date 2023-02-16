969eeba0664263bb5e96ee2b1cde78235720fc15
Andentu <severalmaz97@gmail.com>
2023-02-16 21:47:51 +0300

    Добавил код для получения данных от пользователя в кортедж и вовода их н а экран

--- New_5_1/Program.cs
+++ New_5_1/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace New_5_1
+﻿using System.Reflection.Metadata;
+
+namespace New_5_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            (string Name, string [] Dishes) User;
+            Console.Write("Введите ваше имя: ");
+            User.Name = Console.ReadLine();
+            User.Dishes = new string[5];
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine("Введите ваше любимое блюдо номер {0}", i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+            Console.WriteLine($"Ваше имя {User.Name}");
+            Console.WriteLine("Ваши любимые блюда: ");
+
+            foreach (var Dish in User.Dishes)
+            {
+                Console.WriteLine(Dish);
+            }
+            Console.ReadKey();
         }
     }
 }
